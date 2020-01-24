12cb98d6433e12a8afba0e9cb5148a76b5ea482f
engineerKev <kevin.ruiz518@gmail.com>
2020-01-23 21:16:35 -0800

    updating carousel to pull likes from server and show loading

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import CarouselItem from './CarouselItem/CarouselItem';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Carousel.module.css'
+import * as actions from '../../store/actions/index';
 
 class Carousel extends Component {
     constructor(props) {
@@ -11,6 +14,13 @@ class Carousel extends Component {
             currentActiveIndex: 0
         }
     }
+    componentDidMount() {
+        const { token, userId, fetchedSavedLikes } = this.props
+        if(!fetchedSavedLikes) {
+            this.props.getUserLikes(token, userId);
+        }
+    }
+
     leftArrowClick = () => {
         const { userLikes } = this.props;
 
@@ -29,34 +39,53 @@ class Carousel extends Component {
             currentActiveIndex: newActiveIndex
         });
     };
+
+    componentContent = () => {
+        const { hasLikes, fetchedSavedLikes, userId, userLikes, isLoading } = this.props;
+        const itemStyle = (elemIndex) => elemIndex === this.state.currentActiveIndex ? { display: "block" } : { display: "none" };
+        if (hasLikes || fetchedSavedLikes || userId) {
+            return (isLoading ? <Spinner />
+                :
+                <React.Fragment>
+                    <div className={classes.Container}>
+                        {userLikes.map((testObj, i) => {
+                            return (
+                                <CarouselItem
+                                    title={testObj.title}
+                                    src={testObj.url}
+                                    key={`${testObj.id}_${i}`}
+                                    itemStyle={itemStyle(i)}
+                                />
+                            )
+                        })}
+                        <button type="button" className={classes.Left} onClick={this.leftArrowClick}>&#10094;</button>
+                        <button type="button" className={classes.Right} onClick={this.rightArrowClick}>&#10095;</button>
+                    </div>
+                </React.Fragment>);
+        } else {
+            return <Redirect to="/" />
+        }
+    }
     
     render() {
-        const itemStyle = (elemIndex) => elemIndex === this.state.currentActiveIndex ? { display: "block" } : { display: "none" };
-        const { userLikes } = this.props; 
-        return (
-            <React.Fragment>
-                <div className={classes.Container}>
-                    {userLikes.map((testObj, i) => {
-                        return (
-                            <CarouselItem
-                                title={testObj.title}
-                                src={testObj.url}
-                                key={`${testObj.id}_${i}`}
-                                itemStyle={itemStyle(i)}
-                            />
-                        )
-                    })}
-                    <button type="button" className={classes.Left} onClick={this.leftArrowClick}>&#10094;</button>
-                    <button type="button" className={classes.Right} onClick={this.rightArrowClick}>&#10095;</button>
-                </div>
-            </React.Fragment>
-        );
+        return this.componentContent();
 
     }
 }
 const mapStateToProps = state => {
     return {
-        userLikes: state.likedGiphs.likedGiphs
+        userLikes: state.likedGiphs.likedGiphs,
+        token: state.auth.token,
+        userId: state.auth.userId,
+        isLoading: state.likedGiphs.isLoading,
+        fetchedSavedLikes: state.likedGiphs.fetchedSavedLikes,
+        hasLikes: state.likedGiphs.hasLikes
     }
 }
-export default connect(mapStateToProps)(Carousel);
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getUserLikes: (token, userId) => dispatch(actions.getUserLikes(token, userId))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Carousel);
